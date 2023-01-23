65422586fb71300187402a612b9a1fdc49f8c933
David <david.owuor@student.moringaschool.com>
2023-01-23 16:17:20 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <ul>
+      <li><NavLink to="/" exact4="true" activestyle={{background: "darkblue"}}>Home</NavLink></li>
+      <li>  <NavLink to="/movies" exact="true" activestyle={{background: "darkblue"}}>Movies</NavLink></li>
+      <li> <NavLink to="/directors" exact="true" activestyle={{background: "darkblue"}}>Directors</NavLink></li>
+      <li>  <NavLink to="/actors" exact="true" activestyle={{background: "darkblue"}}>Actors</NavLink></li>
+      </ul>
+    </div>
+  )
 }
 
 export default NavBar;
